dc6dee4058ef2e26d5bf25ab9ebf2446c5c68a23
zenraafif <zenra.afif@gmail.com>
2025-09-10 09:48:06 +0700

    Bank

---
+++ SistemBank/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BankAccount
+{
+    public string Owner { get; }
+    public string NoRek { get; }
+    public string Pajak { get; }
+    public decimal balance;
+
+    public BankAccount(string number, string name, decimal initialBalance)
+    {
+        this.Owner = name;
+        this.NoRek = number;
+
+        Deposit(initialBalance);
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Jumlah deposit minimal Rp.0 !!!");
+            Environment.Exit(1);
+        }
+            
+        balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Jumlah penarikan harus lebih dari Rp.0 !!!");
+            Environment.Exit(1);
+        }
+        if (amount > balance)
+        {
+            Console.WriteLine("Jumlah saldo tidak cukup");
+            Environment.Exit(1);   
+        }
+        balance -= amount;
+    }
+
+    public void PajakTransaksi(decimal amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Transaksi Gagal");
+            Console.WriteLine("Jumlah saldo tidak cukup untuk pajak");
+            Environment.Exit(1);   
+        }
+        balance -= amount;
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+
+    public void DisplayAccount()
+    {
+        Console.WriteLine($"Pemilik: {Owner}, Nomor Akun: {NoRek}, Saldo: {balance:C}, Pajak Transaksi {Pajak}");
+    }
+}
---
+++ SistemBank/Program1.cs
@@ -0,0 +1,20 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== REKENING BANK ===");
+
+        BankAccount akun1 = new BankAccount("1", "Andi", 100_000);
+        akun1.Deposit(1000);
+        akun1.Withdraw(99000);
+        akun1.PajakTransaksi(5099);
+        akun1.DisplayAccount();
+
+        BankAccount akun2 = new BankAccount("2","Siti", 1_000_000);
+        akun2.Withdraw(300_000);
+        akun2.PajakTransaksi(5099);
+        akun2.DisplayAccount();
+
+        Console.WriteLine("Transaksi selesai.");
+    }
+}
